dd179e984508c80874b818609f5dfd7bf45d7ca5
HariboKing <142109124+HariboKing@users.noreply.github.com>
2026-04-17 18:47:54 +0200

    Load deletion request extension in WSGI entrypoint

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,4 @@
+import nlp_trainer_support.deletion_extension  # noqa: F401
 from nlp_trainer_support.runtime_setup import initialize_runtime_database
 from nlp_trainer_support.web import create_app
 
