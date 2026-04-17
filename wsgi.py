911166ab78ab455d5bdc9aab8abbba75da75518f
HariboKing <142109124+HariboKing@users.noreply.github.com>
2026-04-17 18:24:28 +0200

    Initialize runtime database in WSGI entrypoint

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,7 @@
+from nlp_trainer_support.runtime_setup import initialize_runtime_database
 from nlp_trainer_support.web import create_app
 
 
+initialize_runtime_database()
 application = create_app()
 app = application
