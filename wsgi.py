14879b780bc4c81f493e07161e96540aa6a443e5
HariboKing <142109124+HariboKing@users.noreply.github.com>
2026-04-20 03:06:27 +0200

    Load The Game extension in WSGI entrypoint

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,5 @@
 import nlp_trainer_support.deletion_extension  # noqa: F401
+import nlp_trainer_support.game_extension  # noqa: F401
 from nlp_trainer_support.runtime_setup import initialize_runtime_database
 from nlp_trainer_support.web import create_app
 
