32bb38083570a1c94613161e002941c05b3ae9e8
HariboKing <142109124+HariboKing@users.noreply.github.com>
2026-04-27 23:25:51 +0200

    Load content media extension in wsgi entrypoint

--- wsgi.py
+++ wsgi.py
@@ -7,6 +7,7 @@ import nlp_trainer_support.world_models_extension  # noqa: F401
 import nlp_trainer_support.content_admin_extension  # noqa: F401
 import nlp_trainer_support.content_editor_access_extension  # noqa: F401
 import nlp_trainer_support.content_question_management_extension  # noqa: F401
+import nlp_trainer_support.content_media_extension  # noqa: F401
 from nlp_trainer_support.runtime_setup import initialize_runtime_database
 from nlp_trainer_support.web import create_app
 
